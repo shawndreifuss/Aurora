20874372d4ffdc92a36e404c11bf9e1f2807f641
Shawn Dreifuss <shawndreifuss@icloud.com>
2024-05-13 23:16:31 -0700

    favicon

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -9,7 +9,7 @@ const Empty: FC<IEmptyProps> = ({ label }) => {
   return (
     <div className="h-full p-20 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
-        <Image alt="Empty" fill src="/aurora.png" />
+        <Image alt="Empty" fill src="/aurora-logo.png" />
       </div>
 
       <p className="text-muted-foreground text-sm text-center">{label}</p>
